06d417ac1bfa2ac79ac7e2f0c19397e2f6476d39
sylvanusmorales-create <sylvanusmorales@gmail.com>
2026-07-16 14:29:43 +0100

    Revert: suppression du patch speech_to_text

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,12 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-subprojects {
-    afterEvaluate {
-        if (project.name == "speech_to_text") {
-            android {
-                compileSdk = 34
-            }
-        }
-    }
-}
