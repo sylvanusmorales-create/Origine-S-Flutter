b1f09f0630dc243ca275f28dcf9dd102aa04548a
sylvanusmorales-create <sylvanusmorales@gmail.com>
2026-07-16 19:39:49 +0100

    WebView app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/chat_screen.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const OrigineS());
@@ -14,15 +14,75 @@ class OrigineS extends StatelessWidget {
       title: 'Origine S',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: const ColorScheme.dark(
-          surface: Color(0xFF161616),
-          primary: Color(0xFF4F7EF7),
-          onSurface: Color(0xFFDCDCDC),
-        ),
-        scaffoldBackgroundColor: const Color(0xFF0B0B0B),
-        useMaterial3: true,
+        colorScheme: ColorScheme.dark(surface: Color(0xFF080808)),
+        scaffoldBackgroundColor: const Color(0xFF080808),
       ),
-      home: const ChatScreen(),
+      home: const WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0xFF080808))
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (_) => setState(() => _isLoading = true),
+        onPageFinished: (_) => setState(() => _isLoading = false),
+      ))
+      ..loadRequest(Uri.parse('https://origine-s-production.up.railway.app'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF080808),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            WebViewWidget(controller: _controller),
+            if (_isLoading)
+              const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 32,
+                      height: 32,
+                      child: CircularProgressIndicator(
+                        color: Color(0xFF2563EB),
+                        strokeWidth: 2,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'ORIGINE S',
+                      style: TextStyle(
+                        color: Color(0xFF2563EB),
+                        fontSize: 12,
+                        letterSpacing: 4,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
